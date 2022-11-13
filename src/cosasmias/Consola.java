74a7fb60098b0aa5120cec2c3e12cc30579f6622
Pamela28-hub <disenospg92@gmail.com>
2022-11-13 18:57:31 +0100

    Segundo commit -metodo lligEnter

--- src/cosasmias/Consola.java
+++ src/cosasmias/Consola.java
@@ -13,10 +13,29 @@ import java.io.InputStreamReader;
  * @author PAMELA GARCIA
  */
 public class Consola {
-    
+
     public static String lligText() throws IOException {
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in)); // el lector és el teclat
         return lector.readLine(); // llig el text del teclat
     }
-    
+
+    public static int lligEnter() throws IOException {
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        boolean incorrecte = true;
+        do {
+            try {
+                int valor = Integer.parseInt(lector.readLine()); // transforma el text a enter
+                return valor; // retorna l'enter resultant de la transformació
+            } catch (NumberFormatException ex) { // la transformació falla i s'agafa l'excepció
+            }
+        } while (incorrecte); // es repeteix mentre la transformació falla
+        return -1;
+        
+    /**
+     * lee un entero de teclado. el proceso de lectura se repite mientras el valor no sea un entero
+     * @return el número leído
+     * @lanza IOException
+     */
+    }
+
 }

d80512b20031b11183a705fbf589578d8d95dc72
Pamela28-hub <disenospg92@gmail.com>
2022-11-13 19:55:55 +0100

    cuarto commit

--- src/cambiodebase/Cambiodebase.java
+++ src/cambiodebase/Cambiodebase.java
@@ -16,5 +16,17 @@ public class Cambiodebase {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    public String canviaABase(int base) {
+    if(base<2||base>24){
+    throw new ArithmeticException("la base va de 2 a 24");
+    }
+    String digits = "0123456789ABCDEFGHIJKLMN; // (error 2)
+    String cad = "";
+    while (num > “O”) { // (error 3)
+    cad = cad + digits.charAt(num % base);
+    num = num / base;
+    }
+    return alReves(cad); // crida al mètode alReves
+    }
+
 }
